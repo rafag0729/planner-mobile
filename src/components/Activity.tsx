9f2f29e1d5a4488729236db0ef674df8674517d3
Rafael Gallego <rafael.gallego@olsoftware.com>
2021-09-27 16:00:59 -0500

    SV - Polishing styles on Activity component

--- src/components/Activity.tsx
+++ src/components/Activity.tsx
@@ -9,14 +9,14 @@ import { fontFamily } from '../styles/generalStyles';
 export const Activity = () => {
     return (
         <View style={ activityStyles.container } >
+            <View style={ activityStyles.actionsPosition }>
+                <EditIcon size={ 22 } />
+                <DeleteIcon size={ 22 } />
+            </View>
             <View style={{ flex: 1}}>
                 <Text style={{ ...activityStyles.textFormat, fontFamily: fontFamily.bold }}> Activity </Text>
                 <Text style={{ ...activityStyles.textFormat, fontFamily: fontFamily.bold }}> Activity Type: </Text>
                 <Text style={{ ...activityStyles.textFormat, fontFamily: fontFamily.regular }}> Description of the activity</Text>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'flex-end'}}>
-                <EditIcon size={ 22 } />
-                <DeleteIcon size={ 22 } />
             </View>
         </View>
     )
@@ -25,16 +25,24 @@ export const Activity = () => {
 const activityStyles = StyleSheet.create({
     container: {
         backgroundColor: 'red',
-        height: 150,
+        height: 100,
         position: 'absolute',
         top: 25,
         right: 10,
         left: 10,
-        padding: 2
+        padding: 3
     },
     textFormat: {
         color: 'white',
         fontSize: 14,
         marginVertical: -2
-    }
+    },
+    actionsPosition: {
+        flexDirection: 'row', 
+        justifyContent: 'flex-end',
+        position: 'absolute',
+        right: 3,
+        top: 1
+    },
+    
 });
